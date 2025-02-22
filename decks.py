8c1e86d3c5bbe76a00068a22fc2ffe6c8f0c6f22
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-22 15:16:47 +0100

    Add Italian deck

--- decks.py
+++ decks.py
@@ -4,8 +4,16 @@ DECKS = {
         'field_names': ['Origin', 'Article', 'Usage', 'English'],
         'unique_field': 'Origin',
         'deck_name': 'Words learned - GER',
-        'deck_id': 1736525715371,
+        'deck_id': 1736525715371,  # get_deck_id('Words learned - GER'),
         'model_name': 'German',
-        'model_id': 1736525849539,
+        'model_id': 1736525849539,  # get_model_id('German'),
+    },
+    'Words learned - ITA.csv': {
+        'field_names': ['Origin', 'Gender', 'English', 'Usage'],
+        'unique_field': 'Origin',
+        'deck_name': 'Words learned - ITA',
+        'deck_id': 1740232192117,  # get_deck_id('Words learned - ITA'),
+        'model_name': 'Italian',
+        'model_id': 1740232418670,  # get_model_id('Italian'),
     },
 }
