2bd2c28c6eccbbc16fae46e0342e7294bd8fdb6d
Paripás Viktor <viktor.paripas@kognity.com>
2025-02-23 21:40:32 +0100

    Stop hardcoding deck name, deck ID and model ID

--- decks.py
+++ decks.py
@@ -1,19 +1,24 @@
+from get_deck_id import get_deck_id
+from get_model_id import get_model_id
 
 DECKS = {
     'Words learned - GER.csv': {
         'field_names': ['Origin', 'Article', 'Usage', 'English'],
         'unique_field': 'Origin',
-        'deck_name': 'Words learned - GER',
-        'deck_id': 1736525715371,  # get_deck_id('Words learned - GER'),
         'model_name': 'German',
-        'model_id': 1736525849539,  # get_model_id('German'),
     },
     'Words learned - ITA.csv': {
         'field_names': ['Origin', 'Gender', 'English', 'Usage'],
         'unique_field': 'Origin',
-        'deck_name': 'Words learned - ITA',
-        'deck_id': 1740232192117,  # get_deck_id('Words learned - ITA'),
         'model_name': 'Italian',
-        'model_id': 1740232418670,  # get_model_id('Italian'),
     },
 }
+
+for deck_filename, deck_data in DECKS.items():
+    # The deck name is the same as the filename without the extension.
+    deck_name = deck_filename.split('.')[0]
+    deck_data.update({
+        'deck_name': deck_name,
+        'deck_id': get_deck_id(deck_name),
+        'model_id': get_model_id(deck_data['model_name']),
+    })
