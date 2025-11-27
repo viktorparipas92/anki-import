ee255f4bb8e7bdc68d54557a509f574cd4d5a1f7
Paripás Viktor <viktor.paripas@mividas.com>
2025-11-27 10:29:35 +0100

    Improve variable name

--- create_deck.py
+++ create_deck.py
@@ -1,6 +1,12 @@
 from anki_requests import make_anki_request
 
 
-def create_deck(deck_data):
-    response = make_anki_request('createDeck', params={'deck': deck_data})
+def create_deck(deck_name):
+    response = make_anki_request('createDeck', params={'deck': deck_name})
     return response['result']
+
+
+if __name__ == '__main__':
+    deck_name = 'My New Deck'
+    result = create_deck(deck_name)
+    print(result)
